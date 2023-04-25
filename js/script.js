a3799765b16b48fc6d2ec31cf3838e0959e2e832
DevKyle29 <kylebrentfry@gmail.com>
2023-04-25 21:33:36 +0200

    Update script.js
    task completed

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// Toggle menu for responsive design
+let navLinks = document.getElementById("navLinks");
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
+
+// Slider
+// Sets variables to return inner div and images
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+const prevBtn = document.querySelector("#prevbutton");
+const nextBtn = document.querySelector("#nextbutton");
+// Creates a counter to track which image we are on
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+
+// FUNCTIONS
+// Add a function to track next button clicks
+nextBtn.addEventListener("click", function () {
+  if (counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+});
+//create function to look for PREV button clicks
+prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+});
+// This function is triggered after transformation
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[counter].id === "lastClone") {
+    carouselSlide.style.transition = "none";
+  }
+  if (carouselImages[counter].id === "firstClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
+});
+
+// Scrolling Effects
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Email sent
+function emailsent() {
+  alert("Email recevied, we will be in touch");
+  window.location.href = "index.html";
+}
